6b2c80f3e3ef4076fccfde7a4b3f3037aa8d73a1
Ponter Chia <dozeuto@gmail.com>
2026-02-26 00:54:40 +0800

    Implement main application structure with FastAPI and add initial HTML templates and CSS styles

--- fastapi_blog/main.py
+++ fastapi_blog/main.py
@@ -1,7 +1,12 @@
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
 
 posts: list[dict] = [
     {
@@ -21,10 +26,12 @@ posts: list[dict] = [
 ]
 
 
-@app.get("/", response_class=HTMLResponse, include_in_schema=False)
-@app.get("/posts", response_class=HTMLResponse, include_in_schema=False)
-def home():
-    return f"<h1>{posts[0]['title']}</h1>"
+@app.get("/", include_in_schema=False, name="home")
+@app.get("/posts", include_in_schema=False, name="posts")
+def home(request: Request):
+    return templates.TemplateResponse(
+        request, "home.html", {"posts": posts, "title": "Home"}
+    )
 
 
 @app.get("/api/posts")
