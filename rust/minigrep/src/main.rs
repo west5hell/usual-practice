f0e3e06044e4d3806185438d85b1ba8bb6b41494
Ponter Chia <dozeuto@gmail.com>
2026-05-04 14:22:40 +0800

    feat: Implement search functionality and update main to utilize it

--- rust/minigrep/src/main.rs
+++ rust/minigrep/src/main.rs
@@ -3,18 +3,15 @@ use std::error::Error;
 use std::fs;
 use std::process;
 
+use minigrep::search;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    // let config = parse_config(&args);
-    // let config = Config::new(&args);
     let config = Config::build(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {err}");
         process::exit(1);
     });
-
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
 
     if let Err(e) = run(config) {
         println!("Application error: {e}");
@@ -25,7 +22,9 @@ fn main() {
 fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    println!("With text:\n{}", contents);
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
 
     Ok(())
 }
@@ -36,16 +35,6 @@ struct Config {
 }
 
 impl Config {
-    // fn new(args: &[String]) -> Config {
-    //     if args.len() < 3 {
-    //         panic!("not enough arguments");
-    //     }
-    //     let query = args[1].clone();
-    //     let file_path = args[2].clone();
-
-    //     Config { query, file_path }
-    // }
-
     fn build(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
             return Err("not enough arguments");
@@ -57,10 +46,3 @@ impl Config {
         Ok(Config { query, file_path })
     }
 }
-
-// fn parse_config(args: &[String]) -> Config {
-//     let query = args[1].clone();
-//     let file_path = args[2].clone();
-
-//     Config { query, file_path }
-// }
